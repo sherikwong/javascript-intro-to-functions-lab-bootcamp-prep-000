61b3b7bce6eb290e2da3b3eb34a6af1aa26f7ad9
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-24 18:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,19 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "") {
+    
   }
 }
+
+
+it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
